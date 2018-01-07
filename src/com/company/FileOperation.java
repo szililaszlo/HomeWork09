e3d7ce58200e9ebba0d9f2df8f5e033134b9d722
szililaszlo <laszlo0919@gmail.com>
2018-01-07 21:34:41 +0100

    #1

---
+++ src/com/company/FileOperation.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.io.File;
+
+public class FileOperation {
+
+    //1
+        public static void printDirsInDirectory (String directory) {
+            // a kapott mappa elérést és nevéből File típust hozok létre
+             File folder = new File(directory);
+             //File típusú tömb mely a mappában lévő elemeket tárolja
+             File[] listOfFiles = folder.listFiles();
+
+             try {
+                 //végigmegyek a tömbön
+                 for(File file : listOfFiles){
+                     //ha egy elem mappa akkor kiíratásra kerül
+                     if (file.isDirectory()) {
+                         System.out.println("name: " + file.getName());
+                     }
+                 }
+             }
+             // NullPointer elkapás
+             catch (NullPointerException e) {
+                 e.printStackTrace();
+             }
+             //álltalános hiba elkapás
+             catch (Exception e) {
+                 e.printStackTrace();
+             }
+
+        }
+}
