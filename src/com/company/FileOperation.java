b3b1b8eb7689d44585ee017ec7d60ef9fcc3b1d7
szililaszlo <laszlo0919@gmail.com>
2018-01-07 21:38:05 +0100

    #2

--- src/com/company/FileOperation.java
+++ src/com/company/FileOperation.java
@@ -30,4 +30,30 @@ public class FileOperation {
              }
 
         }
+
+        //2
+        public static void printFilesInDirectory (String directory) {
+            // a kapott mappa elérést és nevéből File típust hozok létre
+            File folder = new File(directory);
+            //File típusú tömb mely a mappában lévő elemeket tárolja
+            File[] listOfFiles = folder.listFiles();
+
+            try {
+                //végigmegyek a tömbön
+                for(File file : listOfFiles){
+                    //ha egy elem fájl akkor kiíratásra kerül
+                    if (file.isFile()) {
+                        System.out.println("name: " + file.getName());
+                    }
+                }
+            }
+            // NullPointer elkapás
+            catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+            //álltalános hiba elkapás
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	//1
-        FileOperation.printDirsInDirectory("E:\\Torrent");
+      //  FileOperation.printDirsInDirectory("E:\\Torrent");
+    //2
+        FileOperation.printFilesInDirectory("E:\\Torrent");
     }
 }
